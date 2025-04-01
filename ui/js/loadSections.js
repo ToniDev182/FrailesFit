5fd3781f76c034825442d8da339746d470c0525a
ToniDev182 <a.atienza.cluster@gmail.com>
2025-04-01 16:54:34 +0200

    dummi commit 15

--- ui/js/loadSections.js
+++ ui/js/loadSections.js
@@ -1,6 +1,12 @@
 const loadComponent = (id, url) => {
-    let basePath = window.location.pathname.includes("/src/pages/") ? "../../" :
-                   window.location.pathname.includes("/build/") ? "../" : "";
+    let basePath = "";
+    
+    // Detectar si estamos en build
+    if (window.location.pathname.includes("/build/")) {
+        basePath = "src/";
+    } else if (window.location.pathname.includes("/src/pages/")) {
+        basePath = "../../";
+    }
 
     const element = document.getElementById(id);
     if (!element || element.innerHTML.trim() !== "") return; // Evita cargarlo dos veces
@@ -16,21 +22,21 @@ document.addEventListener("DOMContentLoaded", () => {
     window.componentsLoaded = true;
 
     const components = [
-        { id: "header", url: "src/components/header.html" },
-        { id: "servicios", url: "src/components/servicios.html" },
-        { id: "nosotros", url: "src/components/nosotros.html" },
-        { id: "galeria", url: "src/components/galeria.html" },
-        { id: "planes", url: "src/components/planes.html" },
-        { id: "footer", url: "src/components/footer.html" },
-        { id: "clases", url: "src/components/clases.html" },
-        { id: "padel", url: "src/components/padel.html" },
-        { id: "pabellon", url: "src/components/pabellon.html" },
-        { id: "gym", url: "src/components/gym.html" },
-        { id: "formulario", url: "src/components/formulario.html" },
-        { id: "entrenador", url: "src/components/entrenador.html" },
-        { id: "muro", url: "src/components/muro.html" },
-        { id: "merch", url: "src/components/merch.html" },
-        { id: "suple", url: "src/components/suple.html" },
+        { id: "header", url: "components/header.html" },
+        { id: "servicios", url: "components/servicios.html" },
+        { id: "nosotros", url: "components/nosotros.html" },
+        { id: "galeria", url: "components/galeria.html" },
+        { id: "planes", url: "components/planes.html" },
+        { id: "footer", url: "components/footer.html" },
+        { id: "clases", url: "components/clases.html" },
+        { id: "padel", url: "components/padel.html" },
+        { id: "pabellon", url: "components/pabellon.html" },
+        { id: "gym", url: "components/gym.html" },
+        { id: "formulario", url: "components/formulario.html" },
+        { id: "entrenador", url: "components/entrenador.html" },
+        { id: "muro", url: "components/muro.html" },
+        { id: "merch", url: "components/merch.html" },
+        { id: "suple", url: "components/suple.html" },
     ];
 
     components.forEach(({ id, url }) => loadComponent(id, url));
